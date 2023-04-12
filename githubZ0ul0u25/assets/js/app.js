f7dec89a2c0b83e1e775382aecc0447c70e325f8
Z0ul0u25 <philougourdo@gmail.com>
2023-04-12 16:41:57 -0400

    modification de l'objet JSON

--- githubZ0ul0u25/assets/js/app.js
+++ githubZ0ul0u25/assets/js/app.js
@@ -9,24 +9,24 @@
 des input[type=radio] */
 const objJSON = {
     "retroactions": {
-        "positive": "Bravo, c’est une bonne réponse !",
-        "negative": "Désolé, ce n’est pas la bonne réponse"
+        "positive": "Bonne réponse !",
+        "negative": "Mauvaise réponse..."
     },
     "explications": {
-        "Q1": "Explications de la question 1",
-        "Q2": "Explications de la question 2",
-        "Q3": "Explications de la question 3"
+        "Q1": "LMG gère officiellement 7 chaîne Youtube afin de couvrir les goût de tout le monde : LinusTechTips, Techlinked, ShortCircuit, Techquickie, LMGClips, Channel Super Fun et MAC address",
+        "Q2": "En date du 27 février 2022, il y a 59 membres officiels au sein de LMG… et ça n’arrête pas d’augmenter!",
+        "Q3": "WAN signifie « Wide Area Network » mieux reconnu comme étant Internet. Le WAN show est présenté depuis plusieurs année par Linus Sebastian et Luke Lafreniere mettant en évidence les nouvelles, petites et grandes, dans le monde des technologies."
     },
     "bonnesReponses": [
-        "Q1A",
-        "Q2C",
-        "Q3B"
+        "Q1C",
+        "Q2B",
+        "Q3A"
     ],
     "messages": {
         "resultatsDebut": "Vous avez obtenu un résultat de",
-        "note0": "Vous auriez pu faire mieux. Je vous suggère ..",
-        "note33": "Vous auriez pu faire mieux. Je vous suggère ..",
-        "note66": "Bravo, vous avez une bonne connaissance générale de ...",
+        "note0": "Vous ne semblez pas connaître LMG, puis-je vous suggéré un détour sur leurs chaînes Youtube?",
+        "note33": "Vous auriez pu faire mieux. Je vous suggère de visiter leurs vidéos sur Youtube",
+        "note66": "Bravo, vous avez une bonne connaissance générale de LMG",
         "note100": "Félicitations, vous êtes un fin connaisseur !"
     }
 };
@@ -36,4 +36,4 @@ const quiz = {
     debuterQuiz: function () { },
     validerReponse: function (idReponse) { },
     afficherResultats: function () { }
-}
+};
