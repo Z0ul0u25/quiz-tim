9fefaac9c94ebccab42d0113e74a230c96ab9ca1
Z0ul0u25 <philougourdo@gmail.com>
2023-04-19 14:31:48 -0400

    Meilleur gestion des affichage de question.

--- githubZ0ul0u25/assets/js/app.js
+++ githubZ0ul0u25/assets/js/app.js
@@ -13,14 +13,14 @@ const objJSON = {
         "negative": "Mauvaise réponse..."
     },
     "explications": {
-        "Q1": "LMG gère officiellement 7 chaîne Youtube afin de couvrir les goût de tout le monde : LinusTechTips, Techlinked, ShortCircuit, Techquickie, LMGClips, Channel Super Fun et MAC address",
+        "Q1": "LMG gère officiellement 7 chaîne Youtube afin de couvrir les goût de tout le monde : LinusTechTips, Techlinked, ShortCircuit, Techquickie, LMGClips, Channel Super Fun et MAC address.",
         "Q2": "En date du 27 février 2022, il y a 59 membres officiels au sein de LMG… et ça n’arrête pas d’augmenter!",
         "Q3": "WAN signifie « Wide Area Network » mieux reconnu comme étant Internet. Le WAN show est présenté depuis plusieurs année par Linus Sebastian et Luke Lafreniere mettant en évidence les nouvelles, petites et grandes, dans le monde des technologies."
     },
     "bonnesReponses": [
-        "Q1C",
-        "Q2B",
-        "Q3A"
+        "Q1_C",
+        "Q2_B",
+        "Q3_A"
     ],
     "messages": {
         "resultatsDebut": "Vous avez obtenu un résultat de",
@@ -31,33 +31,62 @@ const objJSON = {
     }
 };
 
-/* Objet Quiz */
-const quiz = {
-    questionCourante: -1,
-    debuterQuiz: function () { },
-    validerReponse: function (idReponse) { },
-    afficherResultats: function () { }
-};
-
 
 const refForm = document.getElementsByTagName("form")[0];
 const refQuestions = document.getElementsByTagName("fieldset");
 const refPrincipal = document.getElementById("principal");
 
+let btnSuivant;
+
+/* Objet Quiz */
+const quiz = {
+    questionCourante: -1,
+    nbBonneReponse: 0,
+    debuterQuiz: function () {
+        refForm.classList.remove("cacher");
+        refPrincipal.classList.add("cacher");
+        refQuestions[++this.questionCourante].classList.remove("cacher");
+    },
+    validerReponse: function (idReponse) {
+        if (idReponse == objJSON.bonnesReponses[this.questionCourante]) {
+            console.log("Bonne réponse!");
+            console.log(refQuestions[this.questionCourante]);
+            refQuestions[this.questionCourante].classList.add("cacher");
+            refQuestions[++this.questionCourante].classList.remove("cacher");
+            // for (const input of document.querySelectorAll(`input[name='Q${quiz.questionCourante + 1}']:not(:checked)+label`) ){
+            //     console.log(input);
+            //     // input.classList.add("grise");
+            // }
+            this.nbBonneReponse++;
+        } else {
+            console.log("mauvaise réponse...");
+        }
+        // btnSuivant.innerText = "Question suivante";
+    },
+    afficherResultats: function () { }
+};
+
 /**
  * Affiche la question suivante et cache la question courrante si valide
  */
-function questionSuivante() {
-    if (quiz.questionCourante++ == -1) {
-        refForm.classList.remove("cacher");
-        refPrincipal.classList.add("cacher");
+function questionSuivante(e) {
+    switch (e.currentTarget.innerText) {
+        case "Commencer le Quiz!":
+            quiz.debuterQuiz();
+            break;
+        case "Vérifier ma réponse": {
+
+            quiz.validerReponse(document.querySelector(`input[name='Q${quiz.questionCourante + 1}']:checked`).value);
+            break;
+        }
+        default:
+            break;
     }
 
-    if (refQuestions[quiz.questionCourante] != undefined){
-        refQuestions[quiz.questionCourante].classList.remove("cacher");
-    }
 
-    (quiz.questionCourante != 0)?refQuestions[quiz.questionCourante - 1].classList.add("cacher"):null;
+    // (quiz.questionCourante != 0)?refQuestions[quiz.questionCourante - 1].classList.add("cacher"):null;
+
+    btnSuivant.setAttribute("disabled", "disabled");
 }
 
 
@@ -82,16 +111,25 @@ function ajoutBtnsInteractif() {
     refPrincipal.appendChild(btn);
 }
 
+function choixReponse() {
+    btnSuivant.removeAttribute("disabled");
+}
+
 /**
  * Action après le chargement de la page.
  */
 function initialisation() {
     // (document.getElementsByTagName("body")[0].classList.contains("js-detector")) ? setupJS() : null;
-    let btnSuivant = document.querySelector("form>button");
-    btnSuivant.innerText = "Question Suivante";
+    btnSuivant = document.querySelector("form>button");
+    btnSuivant.innerText = "Vérifier ma réponse";
+    btnSuivant.setAttribute("disabled", "disabled");
     btnSuivant.addEventListener("click", questionSuivante, false);
     // Évenement pour prévenir l'envoi du formulaire
     refForm.addEventListener("submit", function (e) { e.preventDefault(); });
+
+    for (const label of document.getElementsByTagName("label")) {
+        label.addEventListener("click", choixReponse, false);
+    }
 
     cacherForm();
     ajoutBtnsInteractif();
